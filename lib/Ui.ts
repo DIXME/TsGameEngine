b842c024a3be593a835167fe810b879df685fcb4
= <=>
2025-04-27 16:57:17 -0400

    old (form old device)

--- lib/Ui.ts
+++ lib/Ui.ts
@@ -1,3 +1,9 @@
+import { KeyboardManager } from "./Keyboard";
+
 export class ui {
-    
+    // this class will manage ui
+    // the "ui" for now will just be an html overlay but
+    // i might just make my own ui stuff from scartch idk
+
+    constructor (keyboard: KeyboardManager){}
 }
